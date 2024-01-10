c91e90a60c49093dc924126dec8a90cd235fe20c
dev2gahui <dev2gahui@gmail.com>
2024-01-10 19:22:59 +0900

    Init: 프로젝트 초기 생성

---
+++ src/main/java/com/charmd/hediz/HedizApplication.java
@@ -0,0 +1,14 @@
+package com.charmd.hediz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HedizApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HedizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/charmd/hediz/HedizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charmd.hediz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HedizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
