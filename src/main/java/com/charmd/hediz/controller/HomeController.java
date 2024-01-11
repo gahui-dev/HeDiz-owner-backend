1eb88d53b07fec05c82ccc44ffb4e9dc24c05ac6
Chanwoong Park <cksdnddlek99@gmail.com>
2024-01-11 19:53:47 +0900

    Test: github test

--- src/main/java/com/charmd/hediz/controller/HomeController.java
+++ src/main/java/com/charmd/hediz/controller/HomeController.java
@@ -12,7 +12,7 @@ public class HomeController {
 
     @GetMapping("dashboard")
     public String dashboard(){
-        return "대시보드 페이지";
+        return "대시보드 페이지11";
     }
 
     @GetMapping("realtime-reservation")
